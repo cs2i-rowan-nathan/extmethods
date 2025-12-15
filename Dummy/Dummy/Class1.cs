9498e9fd0a025edfbfa4c27e975f83ad92d62c95
nathan <ncr1989@gmail.com>
2025-12-15 09:39:42 +0100

    intial commit

---
+++ Dummy/Dummy/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Dummy.Extensions
+{
+    public static class IntExtensions
+    {
+        public static bool NotEven(this int number)
+        {
+            return number % 2 != 0;
+        }
+    }
+}
---
+++ TestProject1/TestProject1/Test1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dummy.Extensions;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class IntExtensionsTests
+    {
+        [TestMethod]
+        public void NotEven_ReturnsFalse_ForEvenNumber()
+        {
+            int number = 4;
+
+            bool result = number.NotEven();
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void NotEven_ReturnsTrue_ForOddNumber()
+        {
+            int number = 5;
+
+            bool result = number.NotEven();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
+
